4793ae927c0319122390c12a1ec0a59810f87e8d
chocobe <mjh2903@naver.com>
2019-09-14 02:41:39 +0900

    카메라 이동

---
+++ TestProject/Assets/Scripts/CameraMove.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMove : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject player;
+    public Transform transform;
+    private Vector3 pos;
+    void Start()
+    {
+        transform = GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        pos.Set(player.transform.position.x, transform.position.y, player.transform.position.z);
+        transform.SetPositionAndRotation(pos, transform.rotation);
+        
+    }
+}
